ff0a8ef0a064f2355189a380bb6e81df91a9a38a
Anamul haque <md.helpoperation@gmail.com>
2026-09-03 16:10:10 +0600

    fix: revert production crash - remove DemoDataCleanupModule and restore Dockerfile

--- apps/api/src/common/cleanup/demo-cleanup.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DbModule } from 'src/db/db.module';
-import { DemoDataCleanupService } from './demo-cleanup.service';
-
-@Module({
-  imports: [DbModule],
-  providers: [DemoDataCleanupService],
-})
-export class DemoDataCleanupModule {}
